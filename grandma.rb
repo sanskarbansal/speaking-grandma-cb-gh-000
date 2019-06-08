b28daa349682e6c792cee9f8ef01fb564ab2760e
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-08 06:59:42 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,10 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  phrase.all?{ |va| val.is_upper? }
+  if(!phrase.all?{ |va| val.is_upper? })
+    return "HUH?! SPEAK UP, SONNY!"
+  else
+    return "NO, NOT SINCE 1938!"
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
