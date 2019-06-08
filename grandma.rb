47a56ee5d2504b6ddfe8fbe181b9e263c75a9f46
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-08 07:01:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-  if(!phrase.all?{ |va| val.is_upper? })
-    return "HUH?! SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  elsif(!phrase.all?{ |va| val.is_upper? })
+    return "HUH?! SPEAK UP, SONNY!"
   else
     return "NO, NOT SINCE 1938!"
   end
