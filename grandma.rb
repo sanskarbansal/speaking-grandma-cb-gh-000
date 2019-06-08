1ee8bc4d1daa6978277c2343ef740a11ff595256
Sanskar Bansal <sanskarbnsl75@gmail.com>
2019-06-08 07:00:30 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,8 @@
 def speak_to_grandma(phrase)
   if(!phrase.all?{ |va| val.is_upper? })
     return "HUH?! SPEAK UP, SONNY!"
+  elsif phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     return "NO, NOT SINCE 1938!"
   end
